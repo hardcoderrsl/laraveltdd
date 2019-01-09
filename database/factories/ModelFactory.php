5e3573e65623e12aab95598d895400c91d4c9ecb
unknown <hardcoderrsl@gmail.com>
2019-01-09 15:30:10 +0600

    first commit

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+use Faker\Generator as Faker;
+
+// $factory->define(Model::class, function (Faker $faker) {
+//     return [
+//         //
+//     ];
+// });
+
+
+
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence(),
+        'body' => $faker->paragraph()
+    ];
+});
+
+
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        
+        'thread_id' => function(){
+            return factory('App\Thread')->create()->id;
+        },
+        
+        'body' => $faker->paragraph()
+    ];
+});

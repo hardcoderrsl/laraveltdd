ff69aa83e9389100ecffd2b8b7e810336a245faf
rassel mahmud <hardcoderrsl@gmail.com>
2019-01-10 14:53:01 +0600

    replies are visible with threads now, replies-owner(user) relationship added, unit test added to check if reply has owner or not

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,23 +23,26 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+       <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Replies</div>
-
-                <div class="card-body">
-                   @foreach($thread->replies as $reply)
-                    <article>
-                     
-                     <div class="body">{{$reply->body}}</div>
-                     
-                    </article>
-                    <hr>
-                   @endforeach
+                <div class="card-header">Replies</div> 
+            @foreach($thread->replies as $reply)   
+            <div class="card">
+                <div class="card-header">
+               <a href="#">{{$reply->owner->name}}</a>  said- 
+                {{$reply->created_at->diffForHumans()}}
                 </div>
+                <div class="card-body">{{$reply->body}}</div>
             </div>
+                    <br>
+            @endforeach               
+            </div>
+
+
         </div>
     </div>
-</div>
+
+
 @endsection
 
+</div>

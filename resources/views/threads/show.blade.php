9cd7fcf8c3d6fdda7afaf17821c45f0182831c29
rassel mahmud <hardcoderrsl@gmail.com>
2019-01-15 12:21:37 +0600

    add reply to thread from thread details page added

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,9 +19,10 @@
             </div>
         </div>
     </div>
+    <br>
     <div class="row justify-content-center">
        <div class="col-md-8">
-            <div class="card">
+            <!-- <div class="card"> -->
                 <div class="card-header">Replies</div> 
             @foreach($thread->replies as $reply)   
             <div class="card">
@@ -33,13 +34,30 @@
             </div>
                     <br>
             @endforeach               
-            </div>
+            <!-- </div> -->
 
 
         </div>
     </div>
+    @if(auth()->check())
+    <div class="row justify-content-center">
+       <div class="col-md-8">
+            <form action="{{$thread->path().'/replies'}}" method="POST">
+            @csrf
+                <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" placeholder="Have someting to say?" cols="30" rows="10"></textarea>
+                </div>
 
+                <button type="submit" class="btn btn-default">Post</button>
+            </form>
+            
 
+        </div>
+    </div>
+
+    @else
+      <p class="text-center"> <a href="{{route('login')}}">please login</a> to participate conversation.</p>
+    @endif
 @endsection
 
 </div>
